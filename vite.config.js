b935f47abc386a27c47fb06e9d4eb1e18d3e644f
Michael <inyomanmichaelsuputra@gmail.com>
2025-05-17 21:45:11 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    cors: {
+      origin: ['gis_2205551156.manpits.xyz', 'http://localhost:5173'],
+			methods: ['GET', 'POST'],
+			allowedHeaders: ['Content-Type']
+    },
+    allowedHosts: ['gis_2205551156.manpits.xyz']
+  }
 })
